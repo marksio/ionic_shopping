532e43fb9eaa3e5d4a8dcf691197ec72fd2a480d
marksio <15073448@imail.sunway.edu.my>
2018-01-18 11:47:36 +0800

    Fix image enlargement & add passing value to view detail of item

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { Deal } from '../deal/deal';
 import { Cart } from '../cart/cart';
 import { Menu } from '../menu/menu';
+import { globalVariable } from '../../providers/globalVariable';
 
 @Component({
   selector: 'page-home',
@@ -16,6 +17,9 @@ export class HomePage {
   items: Object[] = []
   itemsInCart: Object[] = []
 
-  constructor(public navCtrl: NavController) {
-  }    
+  badgeCount: any;
+
+  constructor(public navCtrl: NavController, public globalVariable: globalVariable) {
+    this.globalVariable.cartSumCount = 0;
+  }
 }
