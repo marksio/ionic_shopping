fdd0960c4b0e3421d875964b91a5bbfef669dfbf
marksio <15073448@imail.sunway.edu.my>
2018-01-19 12:02:15 +0800

    Change cart

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -24,46 +24,50 @@ export class Detail {
     let id = navParams.get('id');
     let quantityInCart = navParams.get('quantityInCart');
     let sum = navParams.get('sum');
+    let title = navParams.get('title');
+    let img = navParams.get('img');
+    let price = navParams.get('price');
+    
     // allItem(id);
     switch (id) {
       case 1 : {
-        this.items = {id: 1, img: 'http://lorempixel.com/200/200', title: 'T-Shirt', price: 132, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "10% off", np: 135 };
+        this.items = {id: 1, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "10% off", np: 135 };
         break;
       }
       case 2 : {
-        this.items = {id: 2, img: 'http://lorempixel.com/201/201', title: 'Smart Phone', price: '1699', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "Buy 3 Free 1", np: "" };
+        this.items = {id: 2, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "Buy 3 Free 1", np: "" };
         break;
       }
       case 3 : {
-        this.items = {id: 3, img: 'http://lorempixel.com/202/202', title: 'Camera', price: '123', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
+        this.items = {id: 3, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
         break;
       }
       case 4 : {
-        this.items = {id: 4, img: 'http://lorempixel.com/203/203', title: 'T-Shirt', price: '132', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "15% off", np: "145" };
+        this.items = {id: 4, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "15% off", np: "145" };
         break;
       }
       case 5 : {
-        this.items = {id: 5, img: 'http://lorempixel.com/204/204', title: 'Smart Phone', price: '1699', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
+        this.items = {id: 5, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
         break;
       }
       case 6 : {
-        this.items = {id: 6, img: 'http://lorempixel.com/205/205', title: 'Camera', price: '123', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "30% off", np: "160" };
+        this.items = {id: 6, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "30% off", np: "160" };
         break;
       }
       case 7 : {
-        this.items = {id: 7, img: 'http://lorempixel.com/206/206', title: 'T-Shirt', price: '132', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
+        this.items = {id: 7, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
         break;
       }
       case 8 : {
-        this.items = {id: 8, img: 'http://lorempixel.com/207/207', title: 'Smart Phone', price: '1699', desc: 'Very Good', currency: "RM" , quantityInCart: quantityInCart, sum: sum, promo: "Buy 1 Free 1", np: "" };
+        this.items = {id: 8, img: img, title: title, price: price, desc: 'Very Good', currency: "RM" , quantityInCart: quantityInCart, sum: sum, promo: "Buy 1 Free 1", np: "" };
         break;
       }
       case 9 : {
-        this.items = {id: 9, img: 'http://lorempixel.com/208/208', title: 'Camera', price: '123', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
+        this.items = {id: 9, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
         break;
       }
       case 10 : {
-        this.items = {id: 10, img: 'http://lorempixel.com/209/209', title: 'iPhone', price: '3424', desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
+        this.items = {id: 10, img: img, title: title, price: price, desc: 'Very Good', currency: "RM", quantityInCart: quantityInCart, sum: sum, promo: "", np: "" };
         break;
       }
     }
@@ -119,10 +123,9 @@ export class Detail {
   //   }
   // }
 
-  addToCart(id) {
-    this.items.quantityInCart += 1;
-    this.globalVariable.cartSumCount += 1;
-    this.items.sum = this.items.price*this.items.quantityInCart;
+  addCart(items) {
+    items.quantityInCart += 1;
+    items.sum = items.price*items.quantityInCart;
     let toast = this.toastCtrl.create({
       message: 'Added to Cart',
       duration: 500,
@@ -130,11 +133,15 @@ export class Detail {
     });
     toast.present();
   }
+
+  add (id, title, img, quantityInCart, sum, price) {
+    this.globalVariable.cartSumCount += 1;
+    console.log(this.globalVariable.cart=[[id],[title],[img],[quantityInCart], [sum], [price]]);
+  }
   
-  reduceCart(id) {
-    this.items.quantityInCart -= 1;
-    this.globalVariable.cartSumCount -= 1;
-    this.items.sum = this.items.price*this.items.quantityInCart;
+  reduceCart(items) {
+    items.quantityInCart -= 1;
+    items.sum = items.price*items.quantityInCart;
     let toast = this.toastCtrl.create({
       message: 'Remove from Cart',
       duration: 500,
@@ -143,4 +150,8 @@ export class Detail {
     toast.present();
   }
 
+  reduce (id, title, img, quantityInCart, sum, price) {
+    this.globalVariable.cartSumCount -= 1;
+    console.log(this.globalVariable.cart=[[id],[title],[img],[quantityInCart], [sum], [price]]);
+  }
 }
